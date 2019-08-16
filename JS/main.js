d776d620050e494af21f49af21982d45f437aad6
VisionTan <tiling.tan@foxmail.com>
2019-08-16 16:33:31 +0800

    调速

--- JS/main.js
+++ JS/main.js
@@ -11,7 +11,7 @@ function writeCode(prefix, code, fn) {
             window.clearInterval(id)
             fn.call()
         }
-    }, 0)
+    }, 50)
 }
 function writeMarkdown(markdown,fn) { 
     let domPaper = document.querySelector('#paper > .content')
@@ -24,7 +24,7 @@ function writeMarkdown(markdown,fn) {
             window.clearInterval(id)
             fn.call()
         }
-    }, 0)
+    }, 50)
 }
 
 
